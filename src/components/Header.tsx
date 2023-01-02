cda2ad1fe8c7e02e242c345f8f931e0e3f270802
AnupWiseSkulls7 <anup@wiseskulls.com>
2023-01-02 16:57:02 +0530

    anup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,62 +1,90 @@
 /* eslint-disable react/no-unescaped-entities */
-import Link from "next/link";
-import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
-import { Container, Nav, Navbar } from "react-bootstrap";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import Image from 'next/image';
+import Logo from "../../public/AnupSolanki.svg";
+import navIcon1 from "../../public/nav-icon1.svg";
+import navIcon2 from "../../public/nav-icon2.svg";
+import navIcon3 from "../../public/nav-icon3.svg";
+// import { HashLink } from "react-router-hash-link";
 
 const Header = () => {
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (value: any) => {
+    setActiveLink(value);
+  };
+
   return (
-    <Navbar expand="lg" sticky="top">
+    <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
       <Container>
-        <Navbar.Brand href="/" style={{ margin: "0px" }}>
-          <img src="/AnupSolanki.svg" width="100%" height="50%" alt="" />
+        <Navbar.Brand href="/">
+          <Image alt="" src={Logo} />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
-          <Nav className="mb-4">
-            <li className="nav-item">
-              <div className="tabs col">
-                <ul>
-                  <li>
-                    {" "}
-                    <Link href="/"> Home </Link>
-                  </li>
-                  <li>
-                    {" "}
-                    <Link href="/skills"> Skills </Link>
-                  </li>
-                  <li>
-                    {" "}
-                    <Link href="/projects"> Projects </Link>
-                  </li>
-                </ul>
-              </div>
-            </li>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link
+              href="#home"
+              className={
+                activeLink === "home" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("home")}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#skills"
+              className={
+                activeLink === "skills" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("skills")}
+            >
+              Skills
+            </Nav.Link>
+            <Nav.Link
+              href="#projects"
+              className={
+                activeLink === "projects" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("projects")}
+            >
+              Projects
+            </Nav.Link>
           </Nav>
-          <Nav className="mb-4">
-            <li className="nav-item">
-              <div className="socials navbar-nav mr-auto">
-                <ul>
-                  <li className="linked">
-                    <FaLinkedinIn />
-                  </li>
-                  <li className="insta">
-                    <FaInstagram />
-                  </li>
-                  <li className="facebook">
-                    <FaFacebookF />
-                  </li>
-                </ul>
-              </div>
-            </li>
-          </Nav>
-          <Nav className="mb-4">
-            <li className="nav-item">
-              <div className="action-btn navbar-nav mr-auto">
-                <button> Let's Connect</button>
-              </div>
-            </li>
-          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="#">
+              <Image alt="" src={navIcon1} />
+              </a>
+              <a href="#">
+              <Image alt="" src={navIcon2} />
+              </a>
+              <a href="#">
+              <Image alt="" src={navIcon3} />
+              </a>
+            </div>
+              <button className="vvd">
+                <span>Let’s Connect</span>
+              </button>
+          </span>
         </Navbar.Collapse>
       </Container>
     </Navbar>
