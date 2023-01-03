68d1cb021f3d5772cef0c30610692acecca0e9c8
AnupWiseSkulls7 <anup@wiseskulls.com>
2023-01-03 09:23:58 +0530

    anup

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
   const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
-  const period = 2000;
+  const period = 200;
 
   useEffect(() => {
     let ticker = setInterval(() => {
@@ -43,7 +43,7 @@ export default function Home() {
       setIsDeleting(false);
       setLoopNum(loopNum + 1);
       setIndex(1);
-      setDelta(500);
+      setDelta(50);
     } else {
       setIndex(prevIndex => prevIndex + 1);
     }
@@ -58,7 +58,7 @@ export default function Home() {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Anup`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "React Js Dev", "FrontEnd Dev" ]'><span className="wrap">{text}</span></span></h1>
+                <h1>{`Hi! I'm Anup`} <span className="txt-rotate" data-rotate='[ "Web Developer", "React Js Dev", "FrontEnd Dev" ]'><span className="wrap">{text}</span></span></h1>
                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
