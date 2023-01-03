57981f75457bd3ced44cccc5c9a02f69e626cdd4
AnupWiseSkulls7 <anup@wiseskulls.com>
2023-01-03 10:07:50 +0530

    anup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,6 @@ import Header from "../src/components/Header";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-    <Header/>
       <Component {...pageProps} />
     </>
   );
