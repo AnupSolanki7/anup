4a39551be30e2463b4e6e49dbea2ead8f9b098bc
AnupWiseSkulls7 <anup@wiseskulls.com>
2023-01-05 11:49:30 +0530

    anup

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -83,13 +83,6 @@ const Contact = () => {
           <Col size={12} md={6}>
             <TrackVisibility>
               {({ isVisible }) => (
-                // <img
-                //   className={
-                //     isVisible ? "animate__animated animate__zoomIn" : ""
-                //   }
-                //   src={contactImg}
-                //   alt="Contact Us"
-                // />
                 <Image
                   className={
                     isVisible ? "animate__animated animate__zoomIn" : ""
