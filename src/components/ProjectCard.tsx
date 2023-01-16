e9a95b5c07f62ff7e033aaaf208853e50e664b5e
AnupWiseSkulls7 <anup@wiseskulls.com>
2023-01-16 21:13:19 +0530

    anup

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React from "react";
 import { Col } from "react-bootstrap";
-import Image from 'next/image';
+import Image from "next/image";
+import Link from "next/link";
 
-const ProjectCard = ({ title, description, imgUrl }:any) => {
-    return (
-        <Col size={12} sm={6} md={4}>
-          <div className="proj-imgbx">
-            {/* <img src={imgUrl} /> */}
-            <Image src={imgUrl} alt=''/>
-            <div className="proj-txtx">
-              <h4>{title}</h4>
-              <span>{description}</span>
-            </div>
+const ProjectCard = ({ title, description, imgUrl, URL }: any) => {
+  return (
+    <Col size={12} sm={6} md={4}>
+      <Link style={{color:'white'}} href={URL} target="_blank">
+        <div className="proj-imgbx">
+          {/* <img src={imgUrl} /> */}
+          <Image src={imgUrl} alt="" />
+          <div className="proj-txtx">
+            <h4>{title}</h4>
+            <span>{description}</span>
           </div>
-        </Col>
-      )
-}
+        </div>
+      </Link>
+    </Col>
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
